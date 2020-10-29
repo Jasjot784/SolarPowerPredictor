e98656cbf48331de778c0e98a4108326dcea1b56
Jasjot Singh <jasjot784@gmail.com>
2020-10-29 07:57:35 +0530

    Power Output Displayed

--- app/src/main/java/com/jasjotsingh/solarpowerpredictor/PowerOutputActivity.java
+++ app/src/main/java/com/jasjotsingh/solarpowerpredictor/PowerOutputActivity.java
@@ -3,6 +3,7 @@ package com.jasjotsingh.solarpowerpredictor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class PowerOutputActivity extends AppCompatActivity {
@@ -11,6 +12,8 @@ public class PowerOutputActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_power_output);
-        Toast.makeText(this, Double.toString(getIntent().getExtras().getDouble("output")), Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, Double.toString(getIntent().getExtras().getDouble("output")), Toast.LENGTH_SHORT).show();
+        TextView tvoutput = findViewById(R.id.tvOutput);
+        tvoutput.setText(Double.toString(getIntent().getExtras().getDouble("output")));
     }
 }
