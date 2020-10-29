28c9ee77e54cb867be2af59388cb564c49f92039
Jasjot Singh <jasjot784@gmail.com>
2020-10-29 06:08:45 +0530

    Splash Screen Done

--- app/src/main/java/com/jasjotsingh/solarpowerpredictor/MainActivity.java
+++ app/src/main/java/com/jasjotsingh/solarpowerpredictor/MainActivity.java
@@ -2,13 +2,38 @@ package com.jasjotsingh.solarpowerpredictor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import me.wangyuwei.particleview.ParticleView;
 
+public class MainActivity extends AppCompatActivity implements ParticleView.ParticleAnimListener {
+    ParticleView mView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+        animate();
+        listen();
+    }
+
+    public void listen(){
+        mView.setOnParticleAnimListener(this);
+    }
+
+    public void init(){
+        mView = findViewById(R.id.pv_1);
+    }
+
+    public void animate(){
+        mView.startAnim();
+    }
+
+    @Override
+    public void onAnimationEnd() {
+        Intent intent = new Intent(this,DashboardActivity.class);
+        startActivity(intent);
     }
 }
