48678f5960755ee8992c52f0e4891ca2e684e752
Jasjot Singh <jasjot784@gmail.com>
2020-10-29 07:17:12 +0530

    Layout Built for Predict Activity

--- app/src/main/java/com/jasjotsingh/solarpowerpredictor/PredictActivity.java
+++ app/src/main/java/com/jasjotsingh/solarpowerpredictor/PredictActivity.java
@@ -3,12 +3,24 @@ package com.jasjotsingh.solarpowerpredictor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PredictActivity extends AppCompatActivity {
-
+    EditText etTemp,etHumid,etPressure,etCloudCover,etMediumCloud,etShortWaveRadiation,etWindSpeed80,etWindSpeed900,etIncidence,etZenith,etAzimuth;
+    Button mPredict;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_predict);
+
+        init();
+        listen();
+    }
+    public void init(){
+
+    }
+    public void listen(){
+
     }
 }
